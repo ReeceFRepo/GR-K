a77fbf753d2382a66f608528d671d6f17749ccb7
Reece Fryer <60760691+reece1998@users.noreply.github.com>
2022-05-11 11:10:38 +0100

    Update GildedRoseGoogletestUnitTests.cc

--- test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
+++ test/cpp_googletest_unittest/GildedRoseGoogletestUnitTests.cc
@@ -1,15 +1,6 @@
 #include <gtest/gtest.h>
 #include "GildedRose.h"
 
-
-// TEST(GildedRoseTest, Foo) {
-//     vector<Item> items;
-//     items.push_back(Item("Foo", 0, 0));
-//     GildedRose app(items);
-//     app.updateQuality();
-//     EXPECT_EQ("fixme", app.m_Stock[0].m_name);
-// }
-
 TEST(GildedRoseTest, GetItemProperties)
 {
     GildedRose app;
